81dc1922cc1fdc4d496818ee4d6dcd3d304b9ef3
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-23 11:11:33 -0400

    working on SearchItems function in Search component

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -7,3 +7,17 @@ import { saveItemIds, getSavedItemIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_ITEM } from '../utils/mutations';
 
+const SearchItems = () => {
+
+    const [searchedItems, setSearchedItems] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+  
+    // create state to hold saved ItemId values
+    const [savedItemIds, setSavedItemIds] = useState(getSavedItemIds());
+  
+    const [saveItem] = useMutation(SAVE_ITEM);
+}
+
+return ();
+
+export default SearchItems;
