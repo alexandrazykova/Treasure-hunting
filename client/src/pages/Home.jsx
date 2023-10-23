6f2bce91b46abdbfcd5a01288ad67b84c3c2d6bc
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-23 10:35:29 -0400

    added search component index.jsx, added search to home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import Search from "../components/Search";
 
 const Home = () => {
   return (
     <div className="container">
+      <Search />
       <CategoryMenu />
       <ProductList />
       <Cart />
