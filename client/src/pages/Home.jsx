5600af169cd395c9f51f4fab9cdc6cb7b0fe0321
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-24 00:34:50 -0400

    implementing new items feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 import Search from "../components/Search";
+import New from "../components/New";
 
 const Home = () => {
   return (
     <div className="container">
       <Search />
       <CategoryMenu />
+      <New />
       <ProductList />
       <Cart />
     </div>
